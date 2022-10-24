464e16f33c467fc54f4d42756bbb40cb039a5731
mayababu <mayababu73@gmail.com>
2022-10-24 19:16:57 +1300

    Fixed the API error

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # celebrityComparer
 
+## Prerequisites
+1. Python 3.10 or above must be installed
+
+## How To Run Program
+1. Clone repository from GitHub
+2. Create venv on command line in folder (python -m venv venv --upgrade-deps)
+3. Activate venv 
+4. Install requirements (pip install -r requirements.txt)
+5. Set the API key in the environment using the console (set "X-RapidAPI-Key= <get key from Maya>")
+6. Run the server (python manage.py runserver)
+7. On the broswer, use the link http://127.0.0.1:8000/celebrity-ranking to open the program.
+
 ## Setup For Development
 1. Install and activate venv : python -m venv venv --upgrade-deps 
 2. Installing django package : pip install django 
--- celebrityRankingApp/views.py
+++ celebrityRankingApp/views.py
@@ -1,15 +1,15 @@
+# importing libraries needed for the project
 from django.shortcuts import render
 from celebrityRankingApp.forms import InputCelebrityNames
 import requests
 from time import sleep
 import os 
 from operator import itemgetter
-from django.contrib import messages
 
 # Create your views here.
 
 def extractCelebrityData(celebDataDict):
-    celebrityInfoList = []
+    celebrityInfoList = [] # creating the lists with the correct info needed using the keys from the API KVPs
     celebrityInfoList.append(celebDataDict.get('name'))           
     celebrityInfoList.append(celebDataDict.get('age'))           
     celebrityInfoList.append(celebDataDict.get('height'))           
@@ -24,7 +24,7 @@ def getAndRankCelebrities(request):
     celebrityFourDetails = ""
     celebrityFiveDetails = ""
     
-    # creating lists for each celebrity tp ut the wanted information in
+    # creating lists for each celebrity to put the wanted information in
     celebrityOneInfoList = []
     celebrityTwoInfoList = []
     celebrityThreeInfoList = []
@@ -35,7 +35,7 @@ def getAndRankCelebrities(request):
     context = {}
     form = InputCelebrityNames(request.GET or None)
     context['form'] = form
-    if request.method == 'GET':
+    if request.method == 'GET': # checking to see which celebrities have been checked by the user (user input)
         firstCelebrityTicked = request.GET.get('first_celebrity')
         print(f"{firstCelebrityTicked}")
 
@@ -51,15 +51,14 @@ def getAndRankCelebrities(request):
         fifthCelebrityTicked = request.GET.get('fifth_celebrity')
         print(f"{fifthCelebrityTicked}")
 
-        apiErrorFlag = False
+        apiErrorFlag = False # setting a flag to make sure that if an error occurs, it's not the faut of a bad API call.
 
         if firstCelebrityTicked == 'on':
             celebrityData = getCelebrityStats("Benedict Cumberbatch")
-            if celebrityData != "Error":     
+            if celebrityData != "Error": # making sure nothing will continue if there is an error. This code ONLY operates if there are NO errors.    
                 allCelebrityList.append(extractCelebrityData(celebrityData[0]))      
             else:
-                apiErrorFlag = True
-                
+                apiErrorFlag = True # when the flag is set to True, it means that there was an error trying to call the API.
         if secondCelebrityTicked == 'on':
             celebrityData = getCelebrityStats("Tom Hiddleston")
             if celebrityData != "Error":     
@@ -88,13 +87,15 @@ def getAndRankCelebrities(request):
             else:
                 apiErrorFlag = True
 
-        #allCelebrityList = [['Scarlett Johansson', 37, 1.6, 165000000.0], ['Chris Hemsworth', 39, 1.9, 130000000.0], ['Tom Hiddleston', 41, 1.87, 25000000.0]]
+        print(f"{firstCelebrityTicked}, {secondCelebrityTicked}, {thirdCelebrityTicked}, {fourthCelebrityTicked}, {fifthCelebrityTicked}")
+
+        #allCelebrityList = [['Scarlett Johansson', 37, 1.6, 165000000.0], ['Chris Hemsworth', 39, 1.9, 130000000.0], ['Tom Hiddleston', 41, 1.87, 25000000.0]] - previously tried harcoded list to check is the API key was failing
         #allCelebrityList = [['Scarlett Johansson', 37, 1.6, 165000000.0]]
-        if len(allCelebrityList) <= 1:
+        if len(allCelebrityList) <= 1: # making sure the user hasn't inputted one or less celebrities.
             if apiErrorFlag:
-                context['message'] = "Error in API try again later."
+                context['message'] = "Error in API. Please try again." # telling users that it's not their fault the program has crashed, it's an API call fault.
             else : 
-                context['message'] = "Please select two or more celebrities."
+                context['message'] = "Please select two or more celebrities." # letting the user kno that they have to choose two or more celebrities.
         else:
             sortedList = sorted(allCelebrityList, key=itemgetter(3), reverse=True)
             context['inputs'] = sortedList 
@@ -103,22 +104,22 @@ def getAndRankCelebrities(request):
 
 def getCelebrityStats(name):
     try : 
-        apiKey = os.getenv('X-RapidAPI-Key')
+        apiKey = os.getenv('X-RapidAPI-Key') # getting the API key from the compiter system.
         sleep(1)
-        url = "https://celebrityninjas.p.rapidapi.com/v1/searchu"
+        url = "https://celebrityninjas.p.rapidapi.com/v1/search" # this is the website where I got the API key from.
 
         querystring = {"name":name,"limit":"10"}
 
-        headers = {
+        headers = { # creating a dictionary to hold the API key and host in KVP
             "X-RapidAPI-Key": apiKey,
             "X-RapidAPI-Host": "celebrityninjas.p.rapidapi.com"
         }
         response = requests.request("GET", url, headers=headers, params=querystring)
         response.raise_for_status() # Goes straight to the except without executing anything below it if something goes wrong.
-        print(f"PRINTING : {response.text}")
+        #print(f"PRINTING : {response.text}")
         return(response.json())
 
-    except Exception as error :
+    except Exception as error : # checking and printing errors in the console.
         print(f"{error}")
         return("Error")
 
